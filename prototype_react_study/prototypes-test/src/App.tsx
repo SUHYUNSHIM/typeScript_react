3ab224fefb624a45f0e47d6fa2947a1b7c32e6a8
SUHYUNSHIM <lisa9963@naver.com>
2023-05-11 10:15:39 +0900

    prototype 예제

--- prototype_react_study/prototypes-test/src/App.tsx
+++ prototype_react_study/prototypes-test/src/App.tsx
@@ -6,12 +6,13 @@ import Calc from './Calc'
 
 const App = () => {
   const [x, setX] = useState<number>(100);
-  const [y,setY] = useState<number>(200);
-  const [oper,setOper] = useState<string>("+");
+  //const [y,setY] = useState<number>(200);
+  //const [oper,setOper] = useState<string>("+");
 
   return (
     <div>
-      <Calc x={x} y = {y} oper = {oper} />
+      {/* <Calc x={x} y = {y} oper = {oper} /> */}
+      <Calc x = {x}/>
     </div>
   )
 }
--- prototype_react_study/prototypes-test/src/Calc.tsx
+++ prototype_react_study/prototypes-test/src/Calc.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from 'prop-types'
+
 type CalcPropsType = {
     x: number;
     y : number;
@@ -27,6 +29,35 @@ const Calc = (props : CalcPropsType) => {
             </div>
         </div>
     )
+}
+
+const calcChecker = (props: any, propName : string, componentName : string) => {
+    if(propName === "oper"){
+        if(props[propName] !== "+" && props[propName] !== "*"){
+            return new Error(`${propName}속성의 값은 
+            반드시 '+', '*'만 허용합니다(at ${componentName}).`);
+        }
+    }
+    if(propName === "y"){
+        let y = props[propName]; // 전달값[속성 이름]
+        if(y > 100 || y < 0 || y%2 !== 0){
+            return new Error(`${propName}속성의 값은 
+            0이상 100이하의 짝수만 허용합니다.(at ${componentName}).`);
+        }
+    }
+};
+
+//유효성 검증 정보
+Calc.propTypes = {
+    x : PropTypes.number.isRequired,
+    y:  calcChecker,
+    oper : calcChecker,
+};
+
+Calc.defaultProps = {
+    x : 100,
+    y : 20,
+    oper : "+",
 }
 
 export default Calc;
