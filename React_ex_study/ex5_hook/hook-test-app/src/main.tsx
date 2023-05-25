70b8295d720843373bcb34e883177bf66238e44d
SUHYUNSHIM <lisa9963@naver.com>
2023-05-25 19:28:47 +0900

    useMemo, useCallback

--- React_ex_study/ex5_hook/hook-test-app/src/main.tsx
+++ React_ex_study/ex5_hook/hook-test-app/src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client'
 //import App from './App02.tsx'
 //import App from './App03'
 //import App from './App04'
-import App from './App05'
+//import App from './App05'
+//import App from './App06'
+import App from './App07'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
