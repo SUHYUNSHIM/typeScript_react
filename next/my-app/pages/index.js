02f6d94edf30753f66af87dd392153283b430f9c
SUHYUNSHIM <lisa9963@naver.com>
2023-11-25 20:54:51 +0900

    nextjs csr 적용
    -

--- next/my-app/pages/index.js
+++ next/my-app/pages/index.js
@@ -3,19 +3,12 @@ import Head from "next/head"
 import Shim from "@/components/Shim";
 
 
-export default function Home(){
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async () => {
-            const { results } = await (await fetch(`/api/movies`)).json();
-            setMovies(results);
-        })();
-    }, []);
+export default function Home({results}){
+
     return (
         <div className="container">
            <Shim title="Home"/>
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
+            {results?.map((movie) => (
                 <div className= "movie" key={movie.id}>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
                     <h4>{movie.original_title}</h4>
@@ -44,4 +37,12 @@ export default function Home(){
       `}</style>
       </div>
     );
+}
+export async function getServerSideProps(){
+    const {results} = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    return {
+        props : {
+            results,
+        }
+    }
 }
